8bf9cfec722b89684b276e01ed563b3863e39890
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2026-02-03 01:29:46 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,9 @@
 #Setting up Jenkins on ubuntu
 sudo apt update
 sudo apt install fontconfig openjdk-21-jre -y
-java -version
 
-udo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
