dcb9bdf7f899f99d8b245579f7975031c8d17606
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2026-02-03 01:23:42 +0530

    Add Jenkins setup script for Ubuntu
    This script sets up Jenkins on an Ubuntu system by installing necessary packages and adding the Jenkins repository.

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#Setting up Jenkins on ubuntu
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
+
+udo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
