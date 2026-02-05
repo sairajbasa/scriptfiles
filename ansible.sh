9ee0b93b3c412e65b26e5eaeb96e348970215cbe
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2026-02-06 00:11:53 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,2 @@
-#to setup Ansible on Amazon Linux 2
-amazon-linux-extras install ansible2 -y
-yum install python3 python-pip python-dlevel -y
+#to setup Ansible on RedHat
+sudo yum install -y ansible-core
